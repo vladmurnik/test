291cf79f428eda42191352fc2927947c30d87870
vladmurnik <121230377+vladmurnik@users.noreply.github.com>
2022-12-24 20:29:17 +0100

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,40 +1,60 @@
-while True:
-    a = input('1 раздел:  +  ,  -  ,  *  ,  /  ,  :   ')
-    if a == '+':
-            b = float(input('Введите первое число: '))
-            c = float(input('Введите второе число: '))
-            print('Ответ:' + str (b+c))
-    elif a == '-':
-            b = float(input('Введите первое число: '))
-            c = float(input('Введите второе число: '))
-            print('Ответ:' + str (b-c))
-    if a == '*':
-            b = float(input('Введите первое число: '))
-            c = float(input('Введите второе число: '))
-            print('Ответ:' + str (b*c))
-    elif a == '/':
-            b = float(input('Введите первое число: '))
-            c = float(input('Введите второе число: '))
-            print('Ответ:' + str (b/c))
+import math
+t = input("1 , 2 , 3")
+if t == "1":
     while True:
-        if a == '2 раздел':
-            n = input('2 раздел: *? **    ***   √?   : ')
-            if n == '**':
-                b = float(input('Введите число: '))
-                print('Ответ: ' + str(b*b))
-            elif n == '***':
-                b = float(input('Введите число: '))
-                print('Ответ' + str (b*b*b))
-            if n =='√':
-                b = float(input('Введите число: '))
-                f = 1
-                while f*f != b:
-                    f += 1
-                else:
-                    print('Ответ: ' + str (f))
-            if n == '*?':
-                o = int(input('В какой степени? '))
-                e = int(input('Какое число? '))
-                x = e**o
-                print(x)
-
+        a = input('1 section:  +  ,  -  ,  *  ,  /  ,  :   ')
+        if a == '+':
+            b = float(input('Number 1: '))
+            c = float(input('Number 2: '))
+            print("Answer:" + str (b+c))
+        elif a == '-':
+            b = float(input('Number 1: '))
+            c = float(input('Number 2: '))
+            print('Answer:' + str (b-c))
+        if a == '*':
+            b = float(input('Number 1: '))
+            c = float(input('Number 2: '))
+            print("Answer:" + str (b*c))
+        elif a == '/':
+            b = float(input('Number 1: '))
+            c = float(input('Number 2: '))
+            print("Answer:" + str (b/c))
+elif t == "2":
+    while True:
+        if a == '2 section':
+            n = input('2 section: *? **    ***   √?   : ')
+        if n == '**':
+            b = float(input('Number: '))
+            print('Answer: ' + str(b*b))
+        elif n == '***':
+            b = float(input('Number: '))
+            print('Answer' + str (b*b*b))
+        if n =='√':
+            b = float(input('Number: '))
+            f = 1
+            while f*f != b:
+                 f += 1
+            else:
+              print('Answer: ' + str (f))
+        if n == '**?':
+            o = int(input('**? '))
+            e = int(input('Number: '))
+            x = e**o
+            print(x)
+else:
+    while True:
+        l = input("3 section: module , factorial , pi , e :")
+        if l == "module":
+            b = float(input('Number: '))
+            q = math.fabs(b)
+            print(q)
+        elif l == "factorial":
+            b = int(input('Number: '))
+            q = math.factorial(b)
+            print(q)
+        if l == "pi":
+            q = math.pi
+            print(q)
+        elif l == "e":
+            q = math.e
+            print(q)
